5bde56d6b0fc1b2a4ab123c502ab498dc26a6cc8
PowerPixel <52136144+PowerPixel@users.noreply.github.com>
2024-04-29 17:01:59 +0200

    f

--- init_networks.sh
+++ init_networks.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 set -e
 
-if ! which docker; then echo "Docker is not present in your PATH, please install or fix your PATH"; exit 1; fi
+if ! (which docker && (docker --help > /dev/null 2>&1)) ; then echo "Docker is not present in your PATH, please install or fix your PATH"; exit 1; fi
 
 if ! docker ps > /dev/null 2>&1; then 
     echo "Docker cannot be run by the current user. Ensure you have that the docker service is started and that ${USER} is part of the docker group."; 
