e6dd47d4fa44d88e028a3bb905613c9d4a70a974
PowerPixel <52136144+PowerPixel@users.noreply.github.com>
2024-04-29 16:59:01 +0200

    stack/auth: remove ory stack for authelia

---
+++ postgres/docker-entrypoint-initdb.d/create-user-authelia.sql
@@ -0,0 +1,5 @@
+CREATE USER authelia WITH PASSWORD 'authelia';
+CREATE DATABASE authelia;
+GRANT ALL PRIVILEGES ON DATABASE authelia TO authelia;
+\c authelia
+GRANT ALL ON SCHEMA authelia TO authelia;
