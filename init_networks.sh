966d3634cdf79c31fd121ec4585ffa306aacff1c
PowerPixel <52136144+PowerPixel@users.noreply.github.com>
2024-04-30 23:37:07 +0200

    misc: correction script création networks

--- init_networks.sh
+++ init_networks.sh
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*-
 set -e
 
+create_docker_network() {
+    if ! docker network inspect $1 > /dev/null 2>&1; then
+        docker network create $1 > /dev/null 2>&1;
+    else
+        echo "Network $1 already exists, skipping creation";
+    fi
+}
+
 if ! (which docker > /dev/null 2>&1 && (docker --help > /dev/null 2>&1)) ; then echo "Docker is not present in your PATH, please install or fix your PATH"; exit 1; fi
 
 if ! docker ps > /dev/null 2>&1; then 
@@ -12,3 +20,4 @@ fi
 docker network create oidc;
 docker network create postgres;
 docker network create https_portal;
+docker network create redis;
